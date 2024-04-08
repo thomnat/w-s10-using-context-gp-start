463a38d81319a3fb82dda6825e8bd970765ba057
Natalie Thompson <nataliethompson2314@gmail.com>
2024-04-08 15:30:29 -0700

    used createContext

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,15 @@
-import './styles/reset.css'
-import './styles/styles.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
+import "./styles/reset.css";
+import "./styles/styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import { TodosProvider } from "./context/todos";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 root.render(
-  <App />
-)
+  <TodosProvider>
+    <App />
+  </TodosProvider>
+);
